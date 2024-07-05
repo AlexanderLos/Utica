c210a31530e1e18926fd6205b2ed8d514fcc614b
Illowah <illowahplease@gmail.com>
2024-07-05 12:14:28 -0400

    Adjustments for prod

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -26,7 +26,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Ensure Firebase Analytics is only initialized if supported
+let analytics;
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
