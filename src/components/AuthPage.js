9c87cbf293d3e301ac9d70612d843042e75a97d8
Illowah <illowahplease@gmail.com>
2024-07-05 14:33:10 -0400

    Added Oauth

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -1,6 +1,6 @@
-import { signInWithGooglePopup } from '../utils/firebase.utils';
 import { useAuth } from './AuthContext';
 import { useNavigate } from 'react-router-dom';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 import '../css/AuthPage.css';
 
 const AuthPage = () => {
@@ -9,10 +9,21 @@ const AuthPage = () => {
 
   const handleGoogleSignIn = async () => {
     try {
-      const user = await signInWithGooglePopup();
-      if (user) {
-        login(user);
-        navigate('/map'); 
+      const url = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=token&scope=email profile';
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.openAuth(url, 'YOUR_REDIRECT_URI', {
+          ephemeralWebSession: false,
+        });
+        if (result.type === 'success' && result.url) {
+          const token = new URL(result.url).hash.split('&')[0].split('=')[1];
+          const user = await signInWithGooglePopup(token);
+          if (user) {
+            login(user);
+            navigate('/map'); 
+          }
+        }
+      } else {
+        window.location.href = url;
       }
     } catch (error) {
       console.error("Error signing in with Google:", error);
